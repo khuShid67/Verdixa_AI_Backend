8f686aa9c602696b4338ffd3ffea50f4d256f1e1
Khushi <dodiyakhushi2512@gmail.com>
2026-06-13 00:56:23 +0530

    Move database configuration to environment variables

--- database.py
+++ database.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "mysql+pymysql://root@localhost:3306/verdixa_ai"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
 engine = create_engine(DATABASE_URL)
 
